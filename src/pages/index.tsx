7198619505b5bd9b19a1c81bfea8f395fa08aef8
mansfield_bitter <mansfieldbitter@gmail.com>
2025-11-10 19:52:22 +0000

    feat: update homepage link to 'Contact Us' from tutorial placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,11 +21,11 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          {/*<Link*/}
-          {/*  className="button button--secondary button--lg"*/}
-          {/*  to="/docs/intro">*/}
-          {/*  Docusaurus Tutorial - 5min ⏱️*/}
-          {/*</Link>*/}
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Contact Us
+          </Link>
         </div>
       </div>
     </header>
